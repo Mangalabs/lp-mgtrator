dfa1156def568d826b994be98c6097ee6cfed214
Thicianny <thicianny2@gmail.com>
2026-03-26 22:55:47 -0300

    feat: adicionando tags marketing

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,8 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 
+const GTM_ID = "GTM-PRF758HW";
+
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
@@ -32,9 +34,28 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <link rel="icon" href="/icon.png" />
         <meta name="description" content="MG TratorPeças" />
         <title>MG TratorPeças</title>
+        <Meta />
         <Links />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`,
+          }}
+        />
       </head>
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="gtm"
+          />
+        </noscript>
         {children}
         <ScrollRestoration />
         <Scripts />
